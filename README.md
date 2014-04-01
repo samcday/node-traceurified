04d4364bb911cde752604b74ca7254d6e28db409
Sam Day <sday@atlassian.com>
2014-04-01 17:29:37 +1100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,52 @@
+# traceurified
+
+Easily use Google's awesome-sauce [traceur-compiler](https://github.com/google/traceur-compiler) in your Node projects.
+
+*bootstrap.js*
+```
+require("traceurify")(function() {
+    return true; 
+});
+
+var app = require("./app");
+app.go()
+```
+
+*app.js*
+```
+import {omggenerators} from "./lib";
+
+export var go = function() {
+    return omggenerators().next();
+};
+```
+
+*lib.js*
+```
+export omggenerators = function*() {
+    yield 123;
+}
+```
+
+This library will:
+
+ * Patch your require() to compile your JS using Traceur on the fly.
+ * Patch your Error stacks to be source-mapped and free of Traceur eval() frames.
+
+The second point is the primary utility of this library.
+
+## Installation
+
+```
+npm install traceurified
+```
+
+## API
+
+```
+require("traceurify")(function(file) {
+    return true; 
+});
+```
+
+The main (and only) API for this library. The function passed in is used as a filter to determine if the given `file` path should be compiled using Traceur, or intepreted as vanilla ES5.
---
+++ traceurify.js
@@ -0,0 +1,111 @@
+"use strict";
+
+module.exports = function(filter) {
+    var chain = require("stack-chain");
+    var traceur = require("traceur");
+    var fs = require("fs");
+    var Module = require("module");
+    var SourceMapConsumer = require("source-map").SourceMapConsumer;
+
+    var ErrorReporter = traceur.util.ErrorReporter;
+    var FromOptionsTransformer = traceur.codegeneration.FromOptionsTransformer;
+    var Parser = traceur.syntax.Parser;
+    var SourceFile = traceur.syntax.SourceFile;
+    var TreeWriter = traceur.outputgeneration.TreeWriter;
+
+    var sourceMaps = {};
+    var filters = [];
+    var originalRequireJs = Module._extensions[".js"];
+
+    // Rewrite callsites to:
+    // * use source mapped positions, if available.
+    // * filter out traceurify eval frames.
+    chain.filter.attach(function (error, frames) {
+        return frames.filter(function (callSite) {
+            if (callSite.isEval()) {
+                if (callSite.getEvalOrigin().indexOf("src/node/traceur.js") > -1) {
+                    return false;
+                }
+            }
+
+            var filename = callSite.getFileName();
+            if (!filename) {
+                return true;
+            }
+            var sourceMap = sourceMaps[filename];
+            if (!sourceMap) {
+                return true;
+            }
+
+            var position = {
+                source: filename,
+                line: callSite.getLineNumber(),
+                column: callSite.getColumnNumber()
+            };
+
+            var originalPosition = sourceMap.originalPositionFor(position);
+
+            if (originalPosition && originalPosition.source) {
+                // Patch callSite with the mapped values.
+                Object.defineProperty(callSite, "getLineNumber", {
+                    value: function() {
+                        return originalPosition.line;
+                    }
+                });
+                Object.defineProperty(callSite, "getColumnNumber", {
+                    value: function() {
+                        return originalPosition.column;
+                    }
+                });
+            }
+
+            return true;
+        });
+    });
+
+    function shouldCompile(filename) {
+        if (filters.length === 0) {
+            return true;
+        }
+        for (var i = 0; i < filters.length; i++) {
+            if (filters[i].call(null, filename)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function compile(filename) {
+        traceur.options.modules = "commonjs";
+
+        var contents = fs.readFileSync(filename, "utf-8");
+        var sourceFile = new SourceFile(filename, contents);
+        var parser = new Parser(sourceFile);
+        var tree = parser.parseModule();
+        var reporter = new ErrorReporter();
+        var transformer = new FromOptionsTransformer(reporter);
+        tree = transformer.transform(tree);
+        if (reporter.hadError())
+        throw new Error("Error transforming " + filename);
+
+        var sourceMapGenerator = new traceur.outputgeneration.SourceMapGenerator({ file: filename });
+        var options = { sourceMapGenerator: sourceMapGenerator };
+        var src = TreeWriter.write(tree, options);
+        sourceMaps[filename] = new SourceMapConsumer(options.sourceMap);
+        return src;
+    }
+
+    if (!filter) {
+        filters = [];
+    } else {
+        filters.push(filter);
+    }
+
+    Module._extensions[".js"] = function(module, filename) {
+        if (shouldCompile(filename)) {
+            var source = compile(filename);
+            return module._compile(source, filename);
+        }
+        return originalRequireJs(module, filename);
+    };
+};
