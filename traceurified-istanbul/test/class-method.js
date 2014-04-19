28043b51b45c0dd748e4cd021c63f55d99a17793
Sam Day <sday@atlassian.com>
2014-04-19 19:56:50 +1000

    Fix class test. Add class method test.

--- traceurified-istanbul/test/class-method.js
+++ traceurified-istanbul/test/class-method.js
@@ -2,16 +2,22 @@
 
 import { setupCoverageTest } from "./common";
 
-var code = `class Foo {}`;
+var code = `class Foo {
+  bar() {
+    123;
+  }
+}`;
 
 var expectedCoverage = {
     "file.js": {
         "path": "file.js",
         "s": {
+            "1": 0
         },
         "b": {},
         "f": {
-            "1": 0
+            "1": 0,
+            "2": 0
         },
         "fnMap": {
             "1": {
@@ -28,9 +34,36 @@ var expectedCoverage = {
                         "column": 0
                     }
                 }
+            },
+            "2": {
+                "name": "(anonymous_2)",
+                "line": 2,
+                "loc": {
+                    "start": {
+                        "line": 2,
+                        "column": 2,
+                        "source": "file.js"
+                    },
+                    "end": {
+                        "line": 2,
+                        "column": 8
+                    }
+                }
             }
         },
         "statementMap": {
+          "1": {
+            "start": {
+              "line": 3,
+              "column": 4,
+              "source": "file.js"
+            },
+            "end": {
+              "line": 3,
+              "column": 8,
+              "source": "file.js"
+            }
+          }
         },
         "branchMap": {}
     }
